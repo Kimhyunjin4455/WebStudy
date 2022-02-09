a6a5edb20b9667ec6212ee2b1aefbf37356a27e2
Kimhyunjin4455 <80228088+Kimhyunjin4455@users.noreply.github.com>
2022-02-09 20:20:34 +0900

    Add files via upload

--- MOMENTUM/js/weather.js
+++ MOMENTUM/js/weather.js
@@ -1,8 +1,20 @@
+const API_KEY = "e0a402b25bc393b281d4204e25a399c4"; 
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => { //내용 추출 후 데이터 얻기
+        const weather = document.querySelector("#weather span:first-child");
+        const city  = document.querySelector("#weather span:last-child");
+        weather.innerText = `${data.weather[0].main}/${data.main.temp}`;
+        city.innerText = data.name;ß
+
+    });  
 }
+
 function onGeoError() {
     alert("Can't find you. No weather for you")
 }
